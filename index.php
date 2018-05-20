577fbe9427748004809c00a8bfdd316eb4409980
aniisssh <pokharelanish123@gmail.com>
2018-05-20 08:56:59 +0545

    bootstrap

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>App</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="asssets/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php for($i=1;$i<=10;$i++):?>
     <h1><?php echo "hello world";?></h1>
-<?php endfor;?>
+<?php endfor;?> 
 </body>
 </html>
